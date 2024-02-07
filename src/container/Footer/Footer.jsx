b1c788e95a1d908b6a6d02202b2d0e08e21a5f36
Akash Banik <110094101+0plaze0@users.noreply.github.com>
2024-02-07 19:52:51 +0530

    chores: change email

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card">
           <img src={images.email} alt="email" />
-          <a href="mailto:banikakash131@gmail.com" className="p-text">
+          <a href="mailto:akash.banik.dev@gmail.com" className="p-text">
             akash.banik.dev@gmail.com
           </a>
         </div>
